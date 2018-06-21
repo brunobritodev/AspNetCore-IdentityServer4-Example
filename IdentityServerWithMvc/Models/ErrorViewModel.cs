d243086ea1505803bbc628107c81a4a5df722034
Bruno Brito <bhdebrito@gmail.com>
2018-06-20 23:03:11 -0300

    Add project files.

---
+++ IdentityServerWithMvc/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace IdentityServerWithMvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
